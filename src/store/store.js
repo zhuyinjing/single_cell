e9da80965f99654448f4f4ab268abb35ec29cfb3
zhuyinjing <zhuyinjing@seqsource.com>
2019-04-03 11:37:32 +0800

    modify save svg

--- src/store/store.js
+++ src/store/store.js
@@ -161,16 +161,17 @@ export default new Vuex.Store({
       let id = data[1] ? data[1] : 'd3container'
       var svg = document.getElementById(id);
       var svgElement = svg.getElementsByTagName("svg")[0]
-
-      var svgData = (new XMLSerializer()).serializeToString(svgElement);
-      var a = document.createElement('a');
-      a.href = 'data:image/svg+xml; charset=utf8, ' + encodeURIComponent(svgData.replace(/></g, '>\n\r<'));
-      a.download = data[0] + '.svg';
-
-      //Hack alert: create a temporary link to download the svg
-      document.body.appendChild(a);
-      a.click();
-      document.body.removeChild(a);
+      svgElement.setAttribute("xmlns", "http://www.w3.org/2000/svg");
+      var svgData = svgElement.outerHTML;
+      var preface = '<?xml version="1.0" standalone="no"?>\r\n';
+      var svgBlob = new Blob([preface, svgData], {type:"image/svg+xml;charset=utf-8"});
+      var svgUrl = URL.createObjectURL(svgBlob);
+      var downloadLink = document.createElement("a");
+      downloadLink.href = svgUrl;
+      downloadLink.download = data[0] + '.svg';
+      document.body.appendChild(downloadLink);
+      downloadLink.click();
+      document.body.removeChild(downloadLink);
     },
   }
 })
